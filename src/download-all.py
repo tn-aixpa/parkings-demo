3d6715ce1ca08372073a7f2f5677b7ab4b453822
kazhamiakin <kazhamiakin@gmail.com>
2023-12-22 14:20:07 +0000

    Type fixes

--- src/download-all.py
+++ src/download-all.py
@@ -9,4 +9,7 @@ def downloader(context, url: mlrun.DataItem):
     df = url.as_df(format='csv',sep=";")
     df[['lat', 'lon']] = df['coordinate'].str.split(', ',expand=True)
     df = df.drop(columns=['% occupazione', 'GUID', 'coordinate']).rename(columns={'Parcheggio': 'parcheggio', 'Data': 'data', 'Posti liberi': 'posti_liberi', 'Posti occupati': 'posti_occupati', 'Posti totali': 'posti_totali'})
+    df["lat"] = pd.to_numeric(df["lat"])
+    df["lon"] = pd.to_numeric(df["lon"])
+
     return df
