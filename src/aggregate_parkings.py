2816fedb283ec6d15b5d7dd6a6ac1009ce2f8ee9
kazhamiakin <kazhamiakin@gmail.com>
2023-12-22 14:59:44 +0000

    stats fields

--- src/aggregate_parkings.py
+++ src/aggregate_parkings.py
@@ -8,6 +8,8 @@ def aggregate_parkings(context, di: mlrun.DataItem):
     rdf = di.as_df()
     rdf['data'] = pd.to_datetime(rdf['data'])
     rdf['day'] = rdf['data'].apply(lambda t: t.replace(second=0, minute=0))
+    rdf['hour'] = rdf['day'].dt.hour
+    rdf['dow'] = rdf['day'].dt.dayofweek
     rdf = rdf.drop(columns=['data'])
     grouped = rdf.groupby(['parcheggio','day']).mean()
     df_aggregated = grouped.reset_index()
