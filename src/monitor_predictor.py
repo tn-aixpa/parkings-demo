91afcd3c3cf6d565ca0b5ac431bd797f041e6168
kazhamiakin <kazhamiakin@gmail.com>
2024-02-26 10:13:27 +0100

    sliced data

--- src/monitor_predictor.py
+++ src/monitor_predictor.py
@@ -6,24 +6,44 @@ import pandas as pd
 
 @mlrun.handler()
 def monitor_predictor(context):
+    """
+    Monitor and predict parking occupancy.
+
+    Args:
+    context (MLRunContext): the runtime context object
+    """
+
+    # get project object
     project = context.get_project_object()
 
+    # get serving predictor function
     serving_fn = project.get_function('serving-predictor')
 
+    # get current date and time as string
     date_str = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
+    # get parkings dataset and convert it to a dataframe
     parkings_df = mlrun.get_dataitem('store://datasets/parcheggi/extract-parkings_parkings').as_df()
+
+    # initialize an empty dataframe for predictions
     pred_df = pd.DataFrame(columns=['parcheggio', 'datetime', 'predicted_mean'])
+
+    # iterate over each parking in the dataset
     for index, row in parkings_df.iterrows():
         parking_str = row['parcheggio']
+
+        # construct API URL based on parking and current date
         API_URL = f'https://opendata.comune.bologna.it/api/explore/v2.1/catalog/datasets/disponibilita-parcheggi-storico/records?where=data%3C%3D%27{date_str}%27%20and%20parcheggio%3D%27{parking_str}%27&order_by=data%20DESC&limit=100'
 
+        # define the file to store the latest data
         latest_data_file = 'last_records.json'
 
+        # fetch data from the API and save it to a file
         with requests.get(API_URL) as r:
             with open(latest_data_file, "wb") as f:
                 f.write(r.content)
 
+        # read the latest data from the file and process it
         with open(latest_data_file) as f:
             json_data = json.load(f)
             df_latest = pd.json_normalize(json_data['results']).drop(columns=['guid', 'occupazione']).rename(columns={"coordinate.lon": "lon", "coordinate.lat": "lat"})
@@ -33,6 +53,7 @@ def monitor_predictor(context):
             df_latest = df_latest.drop(columns=['parcheggio'])
             df_latest = df_latest.groupby('date').agg({'value': 'mean'})
 
+        # convert the processed data to JSON and make a request to the serving predictor function
         jsonstr = df_latest.reset_index().to_json(orient='records')
         arr = json.loads(jsonstr)
         res = serving_fn.invoke(path="/v2/models/parcheggi_predictor_model/infer", body={"inputs": arr})
@@ -43,10 +64,23 @@ def monitor_predictor(context):
         res_df = res_df.drop(columns=['date', 'value'])
         pred_df = pd.concat([pred_df, res_df], ignore_index=True)
 
+    # concatenate the predicted results with the existing data (if any) and remove duplicates
     old_pd = pred_df
     try: 
         dat_old = mlrun.get_dataitem('store://datasets/parcheggi/parking_prediction_nbeats_model')
         old_pd = pd.concat([dat_old.as_df(), pred_df], ignore_index=True)
         old_pd = old_pd.drop_duplicates(subset=['parcheggio', 'datetime'])
     except: pass
+
+    # log the predictions as a dataset in the project
     project.log_dataset('parking_prediction_nbeats_model', old_pd, stats=True)
+
+    old_pd = pred_df.copy()
+    old_pd['slice_datetime'] = date_str
+    try:
+        dat_old = mlrun.get_dataitem('store://datasets/parcheggi/parking_prediction_nbeats_model_sliced')
+        old_pd = pd.concat([dat_old.as_df(), old_pd], ignore_index=True)
+    except: pass
+
+    # log the predictions as a dataset in the project
+    project.log_dataset('parking_prediction_nbeats_model_sliced', old_pd, stats=True)
