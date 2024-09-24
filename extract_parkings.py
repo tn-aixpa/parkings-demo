4d89d7789f15021d0c2270a7c1bb53d2f44a9d23
Nawaz Khurshid <nawaz1981@gmail.com>
2024-09-24 12:02:08 +0200

    Fix for moving src files

--- extract_parkings.py
+++
@@ -1,8 +0,0 @@
-from digitalhub_runtime_python import handler
-import pandas as pd
-
-@handler(outputs=["parkings"])
-def extract_parkings(project, di):
-    KEYS = ['parcheggio', 'lat', 'lon', 'posti_totali']
-    df_parcheggi = di.as_df().groupby(['parcheggio']).first().reset_index()[KEYS]
-    return df_parcheggi
